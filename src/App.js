f2aada35594e03f642b48254f87b6c0b859744bd
Sivaranjani Jayaprakash <shiva.ranjani83@gmail.com>
2022-09-22 17:08:57 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Homepage from './components/homepage';
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/homepage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function homepage() {
+    return (
+        <p> Welcome to Sivaranjani Portfolio </p>
+    );
+}
+
+export default homepage;
